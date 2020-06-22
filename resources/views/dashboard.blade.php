0e94ab8842df9151bc0a14447e4bba8b9e13dd50
jomec99 <jelz99@yahoo.com>
2020-06-23 05:36:24 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,43 @@
 @extends('main')
 
+
+
 @section('content')
-    
+
+    <style>
+
+    </style>
+
+    <div class="content-wrapper">
+
+        <section class="content">
+
+
+
+            <div class="box">
+                <div class="box-header">
+                </div>
+                <div class="box-body text-center">
+                    <div class="row-content">
+                        <div class="col-12">
+                            <div class="box">
+                                    <h1>This is the Dashboard</h1>
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </section>
+
+    </div>
+
 @endsection
 
 @section('scripts')
-    
+    <script>
+
+    </script>
 @endsection
